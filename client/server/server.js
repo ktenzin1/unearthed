aa10037ec34d763b91d59746ada50dc5e1d22b8b
ktenzin1 <karmayangchentenzin@gmail.com>
2024-10-01 14:28:37 -0400

    update

--- client/server/server.js
+++ client/server/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 const app = express()
 app.use('/public', express.static('./public'))
+app.use(express.static('./server/public'));      // Serve built files in production
+
 app.use('/scripts', express.static('./public/scripts'))
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
