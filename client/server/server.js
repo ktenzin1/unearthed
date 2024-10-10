a395e9ac9f33a8fa2d44ccf09eefad4885dee426
ktenzin1 <karmayangchentenzin@gmail.com>
2024-10-10 16:55:44 -0400

    update

--- client/server/server.js
+++ client/server/server.js
@@ -2,10 +2,10 @@ import express from 'express'
 import cors from 'cors'
 const app = express()
 app.use(cors())
-app.use('/public', express.static('./public'))
-app.use(express.static('./server/public'));      // Serve built files in production
+//app.use('/public', express.static('./public'))
+//app.use(express.static('./server/public'));      // Serve built files in production
 
-app.use('/scripts', express.static('./public/scripts'))
+//app.use('/scripts', express.static('./public/scripts'))
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
   })
